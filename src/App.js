d9b17aa7b30c41cac6daad81ecf34421bbdae3d7
Utkarsh Tiwari <tiwariutkarsh007@gmail.com>
2024-03-01 17:16:26 +0530

    Add 404 page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-import Navbar from './components/Navbar';
 import List from './pages/List';
 import Details from './pages/Details';
+
+import NotFound from './components/NotFound';
+import Navbar from './components/Navbar';
 
 import './App.css';
 
@@ -14,6 +16,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<List />}></Route>
         <Route path='/details/:id' element={<Details />}></Route>
+        <Route path='*' element={<NotFound />}></Route>
       </Routes>
     </>
   );
