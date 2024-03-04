a17488d1bf3b4ee1996a310c6a6f98cd9a26f587
Utkarsh Tiwari <tiwariutkarsh007@gmail.com>
2024-03-04 19:09:34 +0530

    Remove lazy loading

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
+import ListPage from './pages/ListPage';
+import DetailsPage from './pages/DetailsPage';
+import NotFoundPage from './pages/NotFoundPage';
+
 import Navbar from './components/common/Navbar';
-import Loader from './components/common/Loader';
+// import Loader from './components/common/Loader';
 
 import './styles/global.css';
 
-const ListPage = React.lazy(() => import('./pages/ListPage'));
-const DetailsPage = React.lazy(() => import('./pages/DetailsPage'));
-const NotFoundPage = React.lazy(() => import('./pages/NotFoundPage'));
+// const ListPage = React.lazy(() => import('./pages/ListPage'));
+// const DetailsPage = React.lazy(() => import('./pages/DetailsPage'));
+// const NotFoundPage = React.lazy(() => import('./pages/NotFoundPage'));
 
 const App = () => {
   return (
@@ -18,25 +22,25 @@ const App = () => {
         <Route
           path='/'
           element={
-            <Suspense fallback={<Loader />}>
-              <ListPage />
-            </Suspense>
+            // <Suspense fallback={<Loader />}>
+            <ListPage />
+            // </Suspense>
           }
         ></Route>
         <Route
           path='/details/:id'
           element={
-            <Suspense fallback={<Loader />}>
-              <DetailsPage />
-            </Suspense>
+            // <Suspense fallback={<Loader />}>
+            <DetailsPage />
+            // </Suspense>
           }
         ></Route>
         <Route
           path='*'
           element={
-            <Suspense fallback={<Loader />}>
-              <NotFoundPage />
-            </Suspense>
+            // <Suspense fallback={<Loader />}>
+            <NotFoundPage />
+            // </Suspense>
           }
         ></Route>
       </Routes>
